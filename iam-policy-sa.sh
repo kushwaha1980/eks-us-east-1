a2b4168ef47ade691fe97c59a06f9ee47b738811
kushwaha1980 <ee_deepakkumar@rediffmail.com>
2021-11-22 14:52:40 +0530

    Create iam-policy-sa.sh

---
+++ iam-policy-sa.sh
@@ -0,0 +1,15 @@
+cd /home/ec2-user/eks-us-east-1/resource-api/infra/cloudformation
+chmod +x iam-policy.json
+./iam-policy.json
+
+cd /home/ec2-user/eks-us-east-1/renting-api/infra/cloudformation
+chmod +x iam-policy.json
+./iam-policy.json
+
+cd /home/ec2-user/eks-us-east-1/clients-api/infra/cloudformation
+chmod +x iam-policy.json
+./iam-policy.json
+
+cd /home/ec2-user/eks-us-east-1/inventory-api/infra/cloudformation
+chmod +x iam-policy.json
+./iam-policy.json
