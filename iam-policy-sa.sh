19e380280ff8dc243ab8351393f4758a1773787a
kushwaha1980 <ee_deepakkumar@rediffmail.com>
2021-11-22 15:10:26 +0530

    Update iam-policy-sa.sh

--- iam-policy-sa.sh
+++ iam-policy-sa.sh
@@ -1,15 +1,14 @@
-cd /home/ec2-user/eks-us-east-1/resource-api/infra/cloudformation
-chmod +x iam-policy.json
-./iam-policy.json
+chmod +x create-iam-policy.sh
+./create-iam-policy.sh
 
 cd /home/ec2-user/eks-us-east-1/renting-api/infra/cloudformation
-chmod +x iam-policy.json
-./iam-policy.json
+chmod +x create-iam-policy.sh
+./create-iam-policy.sh
 
 cd /home/ec2-user/eks-us-east-1/clients-api/infra/cloudformation
-chmod +x iam-policy.json
-./iam-policy.json
+chmod +x create-iam-policy.sh
+./create-iam-policy.sh
 
 cd /home/ec2-user/eks-us-east-1/inventory-api/infra/cloudformation
-chmod +x iam-policy.json
-./iam-policy.json
+chmod +x create-iam-policy.sh
+./create-iam-policy.sh
